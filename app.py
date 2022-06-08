efd43687f9d586c641d2aff72cfeb712c31b3233
GeigerTiger55 <elise.geiger@gmail.com>
2022-06-08 16:04:57 -0700

    completed assignment

--- app.py
+++ app.py
@@ -7,3 +7,20 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
+
+@app.get("/questions")
+def create_form():
+    """ Returns form with prompts/text fields for each word needed to generate
+        the story
+
+    """
+    return render_template('questions.html', words=silly_story.prompts)
+
+
+@app.get("/results")
+def generate_story():
+    """ Gets users words from page and returns story text with words filled in
+
+    """
+    story_text = silly_story.generate(request.args)
+    return render_template('story.html', story_text=story_text)
