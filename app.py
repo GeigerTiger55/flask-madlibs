e21d31c032469b53babe69aa150002c23b20645c
GeigerTiger55 <elise.geiger@gmail.com>
2022-06-08 17:29:50 -0700

    removed global variable stuff and began implementing create your own story

--- app.py
+++ app.py
@@ -1,21 +1,20 @@
 from flask import Flask, render_template, request
 from flask_debugtoolbar import DebugToolbarExtension
 
-from stories import silly_story, excited_story
+from stories import story_dictionary
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "secret"
 
 debug = DebugToolbarExtension(app)
 
-STORIES = {"silly": silly_story, "excited": excited_story}
-
 # Default story
 
 @app.get("/home")
 def choose_story():
     """Add docstring"""
-    return render_template('home.html', stories=STORIES)
+    return render_template('home.html', stories=story_dictionary)
+
 
 @app.get("/questions")
 def create_form():
@@ -23,14 +22,35 @@ def create_form():
         the story
 
     """
-    global chosen_story
-    chosen_story = STORIES.get(request.args["chosen_story"])
-    return render_template('questions.html', words=chosen_story.prompts)
+    story_name = request.args["chosen_story"]
+    chosen_story = story_dictionary.get(story_name)
+    return render_template('questions.html', words=chosen_story.prompts, story_name=story_name)
 
-@app.get("/results")
-def generate_story():
+
+@app.get("/results/<story_name>")
+def generate_story(story_name):
     """ Gets users words from page and returns story text with words filled in
 
     """
+    chosen_story = story_dictionary[story_name]
     story_text = chosen_story.generate(request.args)
     return render_template('story.html', story_text=story_text)
+
+
+@app.get("/add_story")
+def make_story():
+    """doc string"""
+    return render_template('add-story.html')
+
+
+@app.post("/home")
+def create_new_story():
+    num_nouns = request.form['number_of_nouns']
+    num_verbs = request.form['number_of_verbs']
+    num_adjs = request.form['number_of_adjectives']
+    num_p_nouns = request.form['number_of_plural_nouns']
+
+    words = []
+
+    #for num in num_nouns:
+    #    words.append({{noun{num}})
